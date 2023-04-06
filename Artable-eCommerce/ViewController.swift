454afc27dfad950e7cd3d6269fd63e91b7a19cb2
kocakaysegul <aysegulkocak112@gmail.com>
2023-04-06 17:58:35 +0300

    updated

--- Artable-eCommerce/ViewController.swift
+++ Artable-eCommerce/ViewController.swift
@@ -12,6 +12,14 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        let storyboard = UIStoryboard(name: "LoginStoryboard", bundle: nil)
+        let controller = storyboard.instantiateViewController(withIdentifier: "loginVC")
+        present(controller, animated: true)
     }
 
 
