96dffa51d16c6538c1fd6189c05d592342a48c5f
kocakaysegul <aysegulkocak112@gmail.com>
2023-04-06 18:10:13 +0300

    files set

---
+++ Artable-eCommerce/Controllers/RegisterVC.swift
@@ -0,0 +1,20 @@
+//
+//  RegisterVC.swift
+//  Artable-eCommerce
+//
+//  Created by Ayşegül Koçak on 6.04.2023.
+//
+
+import UIKit
+
+class RegisterVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
