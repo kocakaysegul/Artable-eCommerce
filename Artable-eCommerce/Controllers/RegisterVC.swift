65602af3b34aaaec235fd0fe1d2c1f7ce7a3392f
kocakaysegul <aysegulkocak112@gmail.com>
2023-04-07 09:37:26 +0300

    new user registered

--- Artable-eCommerce/Controllers/RegisterVC.swift
+++ Artable-eCommerce/Controllers/RegisterVC.swift
@@ -6,9 +6,19 @@
 //
 
 import UIKit
+import Firebase
 
 class RegisterVC: UIViewController {
-
+    
+    
+    @IBOutlet weak var usernameText: UITextField!
+    @IBOutlet weak var emailText: UITextField!
+    @IBOutlet weak var passwordText: UITextField!
+    @IBOutlet weak var confirmPasswordText: UITextField!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -16,5 +26,22 @@ class RegisterVC: UIViewController {
     }
     
 
-
+    @IBAction func registerClicked(_ sender: Any) {
+        
+        guard let email = emailText.text, !email.isEmpty,
+              let username = usernameText.text, !username.isEmpty,
+              let password = passwordText.text, !password.isEmpty else {
+            return
+        }
+        
+        Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+            if let error = error {
+                debugPrint(error)
+                return
+            }
+            print("successfully registered new user")
+        }
+        
+    }
+    
 }
