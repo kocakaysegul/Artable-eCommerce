5cef525336efc3ad515a22f6d667a1fb26ceaf58
kocakaysegul <aysegulkocak112@gmail.com>
2023-04-08 13:23:55 +0300

    logging in users

--- Artable-eCommerce/Controllers/LoginVC.swift
+++ Artable-eCommerce/Controllers/LoginVC.swift
@@ -6,8 +6,16 @@
 //
 
 import UIKit
+import Firebase
 
 class LoginVC: UIViewController {
+    
+    @IBOutlet weak var emailText: UITextField!
+    
+    @IBOutlet weak var passwordText: UITextField!
+    
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -15,7 +23,31 @@ class LoginVC: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
-
+    @IBAction func forgotPasswordClicked(_ sender: Any) {
+        
+    }
+    
+    @IBAction func loginClicked(_ sender: Any) {
+        
+        guard let email = emailText.text, email.isNotEmpty,
+              let password = passwordText.text, password.isNotEmpty else { return }
+        
+        activityIndicator.startAnimating()
+        Auth.auth().signIn(withEmail: email, password: password) { user, error in
+            if let error = error {
+                debugPrint(error)
+                self.activityIndicator.stopAnimating()
+                return
+            }
+            
+            self.activityIndicator.startAnimating()
+            print("Login was successfull")
+        }
+        
+    }
+    
+    @IBAction func guestClicked(_ sender: Any) {
+    }
+    
 
 }
